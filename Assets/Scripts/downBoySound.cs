dcc2f136864a95a64be753c6ffc27709dd23a786
rednaz <zander.nelson@gmail.com>
2014-09-03 19:56:49 -0700

    Initial Commit

---
+++ Assets/Scripts/downBoySound.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class downBoySound : MonoBehaviour 
+{
+
+	bool letsRock = false;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if( letsRock == true )
+		{
+			audio.Play();
+			letsRock = false;
+		}
+	}
+	
+	public void RockIt()
+	{
+		letsRock = true;
+	}
+}
---
+++ Assets/Scripts/yauaSound.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class yauaSound : MonoBehaviour {
+
+	
+	bool letsRock = false;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if( letsRock == true )
+		{
+			audio.Play();
+			letsRock = false;
+		}
+	}
+	
+	public void RockIt2()
+	{
+		letsRock = true;
+	}
+}
